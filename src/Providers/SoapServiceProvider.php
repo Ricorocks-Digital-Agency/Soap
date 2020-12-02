bd960de15f27139dd3b464a36f959aef0f5d78a5
sam <sam@ricorocks.agency>
2020-12-02 17:04:14 +0000

    Adds Fakery class and injects into Soap

--- src/Providers/SoapServiceProvider.php
+++ src/Providers/SoapServiceProvider.php
@@ -8,13 +8,14 @@ use RicorocksDigitalAgency\Soap\Parameters\IntelligentBuilder;
 use RicorocksDigitalAgency\Soap\Request\Request;
 use RicorocksDigitalAgency\Soap\Request\SoapClientRequest;
 use RicorocksDigitalAgency\Soap\Soap;
+use RicorocksDigitalAgency\Soap\Support\Fakery\Fakery;
 
 class SoapServiceProvider extends ServiceProvider
 {
 
     public function register()
     {
-        $this->app->singleton('soap', fn() => app(Soap::class));
+        $this->app->singleton('soap', fn() => app(Soap::class, ['fakery' => app(Fakery::class)]));
         $this->app->bind(Request::class, SoapClientRequest::class);
         $this->app->bind(Builder::class, IntelligentBuilder::class);
     }
--- src/Support/Fakery/Fakery.php
+++ src/Support/Fakery/Fakery.php
@@ -1,81 +1,26 @@
 <?php
 
-
-namespace RicorocksDigitalAgency\Soap;
-
+namespace RicorocksDigitalAgency\Soap\Support\Fakery;
 
 use Closure;
 use Illuminate\Support\Str;
 use PHPUnit\Framework\Assert as PHPUnit;
-use RicorocksDigitalAgency\Soap\Parameters\Node;
 use RicorocksDigitalAgency\Soap\Request\Request;
 use RicorocksDigitalAgency\Soap\Response\Response;
-use RicorocksDigitalAgency\Soap\Support\Fakery\Fakery;
 
-class Soap
+class Fakery
 {
-    protected Fakery $fakery;
-    protected $inclusions = [];
-    protected $recordRequests = false;
+    protected $shouldRecord = false;
     protected $recordedRequests = [];
     protected $stubCallbacks = [];
 
-    public function __construct(Fakery $fakery)
-    {
-        $this->fakery = $fakery;
-    }
-
-    public function to(string $endpoint)
-    {
-        return app(Request::class)
-            ->beforeRequesting(fn($request) => $this->checkForMock($request))
-            ->afterRequesting(fn($request, $response) => $this->record($request, $response))
-            ->to($endpoint);
-    }
-
-    protected function checkForMock(Request $request)
-    {
-        return collect($this->stubCallbacks)
-            ->reverse()
-            ->map
-            ->__invoke($request)
-            ->filter()
-            ->first();
-    }
-
-    public function record(Request $request, Response $response)
-    {
-        if (!$this->recordRequests) {
-            return;
-        }
-
-        $this->recordedRequests[] = [$request, $response];
-    }
-
-    public function node($attributes = []): Node
-    {
-        return new Node($attributes);
-    }
-
-    public function include($parameters)
-    {
-        $inclusion = new Inclusion($parameters);
-        $this->inclusions[] = $inclusion;
-        return $inclusion;
-    }
-
-    public function inclusionsFor(string $endpoint, $method = null)
-    {
-        return collect($this->inclusions)->filter->matches($endpoint, $method);
-    }
-
     public function fake($callback = null)
     {
-        $this->recordRequests = true;
+        $this->shouldRecord = true;
 
         if (is_null($callback)) {
             $this->stubCallbacks = array_merge([fn() => new Response()], $this->stubCallbacks);
-            return app(self::class);
+            return;
         }
 
         if (is_array($callback)) {
@@ -105,15 +50,34 @@ class Soap
         };
     }
 
+    protected function checkForMock(Request $request)
+    {
+        return collect($this->stubCallbacks)
+            ->reverse()
+            ->map
+            ->__invoke($request)
+            ->filter()
+            ->first();
+    }
+
+    public function assertSentCount($count)
+    {
+        PHPUnit::assertCount($count, $this->recordedRequests);
+    }
+
     public function assertNothingSent()
     {
         PHPUnit::assertEmpty($this->recordedRequests, "Requests were recorded");
-        return app(self::class);
     }
 
     public function assertSent(callable $callback)
     {
         PHPUnit::assertTrue($this->recorded($callback)->isNotEmpty());
+    }
+
+    public function assertNotSent(callable $callback)
+    {
+        PHPUnit::assertTrue($this->recorded($callback)->isEmpty());
     }
 
     protected function recorded($callback)
@@ -127,16 +91,5 @@ class Soap
         };
 
         return collect($this->recordedRequests)->filter(fn($pair) => $callback(...$pair));
-    }
-
-    public function assertNotSent(callable $callback)
-    {
-        PHPUnit::assertTrue($this->recorded($callback)->isEmpty());
-    }
-
-    public function assertSentCount($count)
-    {
-        PHPUnit::assertCount($count, $this->recordedRequests);
-        return app(self::class);
     }
 }
