3bebafff46ec385b040ed71cbffa246c952528f4
luke <lukeraymonddowning@gmail.com>
2020-12-03 14:57:20 +0000

    Updates docblock for Facade.

--- src/Facades/Soap.php
+++ src/Facades/Soap.php
@@ -16,7 +16,13 @@ use RicorocksDigitalAgency\Soap\Request\Request;
  * @method static Request to(string $endpoint)
  * @method static Node node(array $attributes = [])
  * @method static include(array $parameters)
+ * @method static \RicorocksDigitalAgency\Soap\Soap beforeRequesting(callable $hook)
+ * @method static \RicorocksDigitalAgency\Soap\Soap afterRequesting(callable $hook)
  * @method static void fake(array|Closure $callable = null)
+ * @method static assertSentCount($count)
+ * @method static assertNothingSent()
+ * @method static assertSent(callable $callback)
+ * @method static assertNotSent(callable $callback)
  */
 class Soap extends Facade
 {
