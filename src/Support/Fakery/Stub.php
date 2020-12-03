57d9f3601855da9b864b261a5ce5b0f1251aae82
sam <nedwors@gmail.com>
2020-12-03 14:15:06 +0000

    Clean up of Stub class

--- src/Support/Fakery/Stub.php
+++ src/Support/Fakery/Stub.php
@@ -29,9 +29,7 @@ class Stub
 
     public function generateResponse(Request $request)
     {
-        $callback = $this->callback;
-
-        return $callback instanceof Closure ? $callback($request) : $callback;
+        return $this->callback instanceof Closure ? call_user_func($this->callback, $request) : $this->callback;
     }
 
     protected function register($endpoint)
@@ -42,17 +40,13 @@ class Stub
             return;
         }
 
-        $details = $this->extractEndpointAndMethods($endpoint);
-        $this->endpoint = $details['endpoint'];
-        $this->methods = $details['methods'];
+        $this->extractEndpointAndMethods($endpoint);
     }
 
     protected function extractEndpointAndMethods($endpoint)
     {
-        return [
-            'endpoint' => (string) Str::of($endpoint)->start('*')->replaceMatches("/:([\w\d]+$)/", ""),
-            'methods' => (string) Str::of($endpoint)->afterLast(".")->match("/:([\w\d]+$)/")->start("*")
-        ];
+        $this->endpoint = (string) Str::of($endpoint)->start('*')->replaceMatches("/:([\w\d]+$)/", "");
+        $this->methods = (string) Str::of($endpoint)->afterLast(".")->match("/:([\w\d]+$)/")->start("*");
     }
 
     public function isForEndpoint($endpoint)
@@ -62,6 +56,6 @@ class Stub
 
     public function isForMethod($method)
     {
-        return $this->methods && Str::is($this->methods, $method);
+        return Str::is($this->methods, $method);
     }
 }
