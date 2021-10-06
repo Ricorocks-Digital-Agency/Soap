0cf54f79d0a4d95557254ff25f4332aff1115316
Luke Downing <lukeraymonddowning@gmail.com>
2021-10-06 16:22:12 +0100

    Initial work

--- src/Contracts/Client.php
+++ src/Contracts/Client.php
@@ -6,17 +6,42 @@ namespace RicorocksDigitalAgency\Soap\Contracts;
 
 interface Client
 {
+    /**
+     * Set the given headers on the request.
+     *
+     * @param array<string, mixed> $headers
+     */
     public function setHeaders(array $headers): static;
 
+    /**
+     * Make a request and return its response.
+     */
     public function call(string $method, mixed $body): mixed;
 
+    /**
+     * Get an array of supported SOAP functions.
+     *
+     * @return array<int, string>
+     */
     public function getFunctions(): array;
 
+    /**
+     * Get the last performed request as an XML string.
+     */
     public function lastRequestAsXml(): ?string;
 
+    /**
+     * Get the last performed response as an XML string.
+     */
     public function lastResponseAsXml(): ?string;
 
+    /**
+     * Get the last performed request headers.
+     */
     public function lastRequestHeaders(): ?string;
 
+    /**
+     * Get the last performed response headers.
+     */
     public function lastResponseHeaders(): ?string;
 }
